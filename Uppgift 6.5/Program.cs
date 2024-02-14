dd433947d8c429154a86905cfec1eb740a69af59
7R1XY <theo.broden@gmail.com>
2024-02-14 13:27:52 +0100

    Add project files.

---
+++ Uppgift 6.5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nu ska du skriva in ord");
+        Console.WriteLine();
+        Console.WriteLine("Skriv");
+        string x = Console.ReadLine();
+        Console.WriteLine("Skriv igen");
+        string y = Console.ReadLine();
+        Console.WriteLine($"Det som var längst var: {Längst(x,y)}");
+        
+        
+        
+    }
+
+    static string Längst(string x, string y)
+    {
+        if (x.Length > y.Length)
+        {
+            return x;
+        }
+        else if (x.Length == y.Length)
+        {
+            return x;
+        }
+        else
+        {
+            return y;
+        }
+    }
+
+    static double Längst(double x, double y)
+    {
+        if (x > y)
+        {
+            return x;
+        }
+        else if (x == y)
+        {
+            return x;
+        }
+        else
+        {
+            return y;
+        }
+    }
+}
